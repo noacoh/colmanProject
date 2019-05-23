34352a2e48e1d3b6e00d0a6c755e5f1efb4d7702
noa <noa@demisto.com>
2019-05-23 22:15:53 +0300

    - fix routing and controller for enlisting user to course

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ router.route('/')
     .post(validateBody(schemas.userSchema),
         UserController.newUser);
 
-router.route(':/userId')
+router.route('/:userId')
     .get(validateParam(schemas.idSchema, 'userId'), UserController.getUser)
     .put(validateParam(schemas.idSchema, 'userId'),
         validateBody(schemas.userSchema),
@@ -18,8 +18,11 @@ router.route(':/userId')
 
 router.route(':/userId/courses')
     .get(validateParam(schemas.idSchema, 'userId'),
-        UserController.getUserCourses)
+        UserController.getUserCourses);
+
+router.route('/:userId/courses/:courseId')
     .post(validateParam(schemas.idSchema, 'userId'),
+        validateParam(schemas.idSchema, 'courseId'),
         UserController.enlistUserToCourse);
 module.exports = router;
 
