0854d87721058ef15b5896f16c887cb59156df22
Liran Ziv <liran27@gmail.com>
2019-06-10 17:04:11 +0300

    compile and run script now supports executing without inputFile

--- docker_sandbox/API/payload/script.sh
+++ docker_sandbox/API/payload/script.sh
@@ -11,11 +11,27 @@
 #
 ########################################################################
 
-compiler=$1
-file=$2
-additionalArg=$3
-output=$4
+#compiler=$1
+#file=$2
+#additionalArg=$3
+#output=$4
 
+CLEAR='\033[0m'
+RED='\033[0;31m'
+
+function usage() {
+  if [[ -n "$1" ]]; then
+    echo -h "${RED}👉 $1${CLEAR}\n";
+  fi
+  echo "Usage: $0 [-c compiler-name] [-f files] [-a additional-args] [-e executable]"
+  echo "  -c, --compiler            Compiler to use"
+  echo "  -s, --files               Files to compile"
+  echo "  -a, --additional          Additional compile args"
+  echo "  -e, --exec                Output file to execute"
+  echo ""
+  echo "Example: $0 --compiler gcc --files "'"*.c *.h"'" --additional -Wall -exec ./a.out"
+  exit 1
+}
 
 ########################################################################
 #	- The script works as follows
@@ -42,18 +58,31 @@ exec  1> $"logfile.txt"
 exec  2> $"errors"
 #3>&1 4>&2 >
 
+while [[ "$#" -gt 0 ]]; do case $1 in
+  -c|--compiler) compiler="$2"; echo compiler:${compiler};  shift;;
+  -f|--files) files="$2"; echo files:${files}; shift;;
+  -a|--additional) additionalArgs="$2"; echo additionalArgs:${additionalArgs};  shift;;
+  -e|--exec) output="$2"; echo output=${output}; shift;;
+  *) usage "Unknown parameter passed: $1"; exit 1;;
+esac; shift; done
+
+# verify params
+if [[ -z "$compiler" ]]; then usage "compiler is not set"; fi;
+if [[ -z "$files" ]]; then usage "files are not set"; fi;
+if [[ -z "$output" ]]; then usage "executable is not set"; fi;
+
 START=$(date +%s.%2N)
 #Branch 1
 if [[ "$output" = "" ]]; then
     if [[ -f "inputFile" ]]; then #check if inputFile exists
-        ${compiler} ${file} -< $"inputFile" #| tee /usercode/output.txt
+        ${compiler} ${files} -< $"inputFile" #| tee /usercode/output.txt
     else
-        ${compiler} ${file}
+        ${compiler} ${files}
     fi
 #Branch 2
 else
 	#In case of compile errors, redirect them to a file
-    ${compiler} ${file} ${additionalArg} #&> /usercode/errors.txt
+    ${compiler} ${files} ${additionalArgs} #&> /usercode/errors.txt
 	#Branch 2a
 	if [[ $? -eq 0 ]];	then
 	    if [[ -f "inputFile" ]]; then #check if inputFile exists
@@ -81,4 +110,3 @@ echo "*-ENDOFOUTPUT-*" ${runtime}
 
 
 mv logfile.txt completed
-
